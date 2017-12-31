e28734416b729e1048a43ba29fde0832a1151f35
mrengel <m.r.engel2010@gmail.com>
2017-12-31 14:13:41 -0500

    Played with Cart Modal some more

--- public/js/med-list.js
+++ public/js/med-list.js
@@ -144,50 +144,14 @@
     medsContainer.append(messageh2);
   }
 
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ["Meds Taken", "Meds Not Taken"],
-        datasets: [{
-            label: 'meds taken',
-            data: [12, 19],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        cutoutPercentage: 50,
-        title: {
-          display: true,
-          fontSize: 20,
-          text: 'Number of Meds Taken Today'
-        }
-    }
-
-  });
+  
   function obtainMedChart(){
     $("#chartModal").modal("toggle");
       var ctx = document.getElementById("myChart").getContext('2d');
       var myDoughnutChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
-          labels: ["Meds Taken", "Meds Not Taken"],
+          labels: ["Doses Taken", "Doses Remaining"],
             datasets: [{
                 label: 'meds taken',
                 data: [12, 19],
@@ -214,8 +178,8 @@
             cutoutPercentage: 50,
             title: {
               display: true,
-              fontSize: 20,
-              text: 'Number of Meds Taken Today'
+              fontSize: 40,
+              text: 'Dose Tracker'
             }
         }
 
