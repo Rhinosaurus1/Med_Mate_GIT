317ec08a4f0edc26781e92d60980f4c66cfd4c63
Jeffrey Allen <allenjeffreyl@gmail.com>
2018-01-07 21:48:40 -0500

    working hybrid

--- server.js
+++ server.js
@@ -4,13 +4,25 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
+var express = require('express')
+var path = require('path')
+var webpack = require('webpack')
 var bodyParser = require("body-parser");
+var passport = require("passport");
+var session = require("express-session");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+var app = express()
+var PORT = process.env.PORT || 3000;
+
+var webpackMiddleware = require("webpack-dev-middleware")
+var webpackConfig = require('./webpack.config.js')
+
+app.use(webpackMiddleware(
+  webpack(webpackConfig),
+  { publicPath: '/' }
+))
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -21,8 +33,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// For Passport
+app.use(session({ 
+  secret: 'keyboard cat',
+  resave: true, 
+  saveUninitialized:true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 // Static directory
 app.use(express.static("public"));
+
+
+app.get('/users', function(req, res){
+  res.sendFile(path.join(__dirname, 'public', 'user-manager.html'))
+});
+
+app.get('/today', function(req, res){
+  res.sendFile(path.join(__dirname, 'public', 'today.html'))
+});
+
+app.get('/med-list', function(req, res){
+  res.sendFile(path.join(__dirname, 'public', 'med-list.html'))
+});
+
+app.get('/med-manager', function(req, res){
+  res.sendFile(path.join(__dirname, 'public', 'med-manager.html'))
+});
+
+/*
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+*/
 
 // Routes
 // =============================================================
@@ -31,9 +75,10 @@ require("./routes/user-api-routes.js")(app);
 require("./routes/meds-api-routes.js")(app);
 require("./routes/events-api-routes.js")(app);
 
+
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Jumbotron = props =>(
+        <div className="jumbotron" id="jumboHome">
+            <div className="container-fluid" id="displayContainer">
+	            <h1 id="heading">MedMate <img id="logoHome" src="images/redcheck.png" alt="" height="200" width="150"/></h1> 
+	            <h3 id="subheading">We remember so you don't have to!</h3>
+	            <div className="panel-body" id="btnPanel">
+		            <a className = "btn btn-info btn-home" href="/users">LOGIN</a>
+		            <a className = "btn btn-info btn-home" href="/users">SIGN UP</a>
+		        </div>
+			</div>
+		</div>
+);
+
+export default Jumbotron;
